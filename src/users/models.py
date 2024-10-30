31f9398faa043bd5c48c5864e6dcccd93cdb735d
Joseph Njoku <oxeejo@gmail.com>
2024-10-30 10:33:59 +0100

    initial-commit

--- src/users/models.py
+++ src/users/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Location(models.Model):
+    address_1 = models.CharField(max_length=128)
+    address_2 = models.CharField(max_length=128, blank=True)
+    city = models.CharField(max_length=64)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     photo = models.ImageField(null=True)
     bio = models.CharField(max_length=140, blank=True)
-    phone_number = models.CharField(max_length=12, blank=True)
+    phone_number = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username}\'s Profile'
