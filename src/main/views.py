05102d3d959095e5ac6f14cd4fbf9191ba3b7123
Joseph Njoku <oxeejo@gmail.com>
2024-10-31 17:39:26 +0100

    initial-commit

--- src/main/views.py
+++ src/main/views.py
@@ -1,5 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def main_view(request):
     return render(request, "views/main.html", {"name": "ShowBlitz"})
+
+def home_view(request):
+    return render(request, "views/home.html")
