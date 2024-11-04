5019431403899206e2f0fb653908cf2c11704725
Joseph Njoku <oxeejo@gmail.com>
2024-11-04 11:12:38 +0100

    initial-commit

--- src/main/models.py
+++ src/main/models.py
@@ -1,9 +1,9 @@
 import uuid
 from django.db import models
 
-from consts import CARS_BRANDS, TRANSMISSION_OPTIONS
+from .consts import CARS_BRANDS, TRANSMISSION_OPTIONS
 from users.models import Profile, Location
-
+from .utils import user_listing_path
 
 
 
@@ -14,13 +14,16 @@ class Listing(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     seller = models.ForeignKey(Profile, on_delete=models.CASCADE)
     brand = models.CharField(max_length=24, choices=CARS_BRANDS, default=None)
-    model = models.CharField(max_length=64,)
-    vin = models.CharField(max_length=17,)
+    model = models.CharField(max_length=64)
+    vin = models.CharField(max_length=17)
     mileage = models.IntegerField(default=0)
-    color = models.CharField(max_length=24)
+    color = models.CharField(max_length=24,)
     description = models.TextField()
-    engine = models.CharField(max_length=24,)
+    engine = models.CharField(max_length=24)
     transmission = models.CharField(
         max_length=24, choices=TRANSMISSION_OPTIONS, default=None)
-    location = models.OneToOneField(Location, on_delete=models.SET_NULL)
-    image = models.ImageField()
+    location = models.OneToOneField(Location, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to=user_listing_path)
+
+    def __str__(self):
+        return f'{self.seller.user.username}\'s Listing - {self.model}'
