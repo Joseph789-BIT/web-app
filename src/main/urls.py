7ef54e9f848d9174b630aa7141ec844b0dd7ea7d
Joseph Njoku <oxeejo@gmail.com>
2024-11-06 13:20:15 +0100

    initial-commit

--- src/main/urls.py
+++ src/main/urls.py
@@ -5,5 +5,5 @@ from .views import main_view, home_view, list_view
 urlpatterns = [
     path('', main_view, name='main'),
     path('home/', home_view, name='home'),
-    path('home/list/', list_view, name='list')
+    path('list/', list_view, name='list')
 ]
