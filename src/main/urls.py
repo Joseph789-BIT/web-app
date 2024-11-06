e403bd4bde06fb5dcc3155c7e4f5e54b1cc8c611
Joseph Njoku <oxeejo@gmail.com>
2024-11-06 11:57:47 +0100

    initial-commit

--- src/main/urls.py
+++ src/main/urls.py
@@ -5,5 +5,5 @@ from .views import main_view, home_view, list_view
 urlpatterns = [
     path('', main_view, name='main'),
     path('home/', home_view, name='home'),
-    path('list/', list_view, name='list')
+    path('home/list/', list_view, name='list')
 ]
--- src/showblit/settings.py
+++ src/showblit/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'showblit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'main/template'),
+        'DIRS': [os.path.join(BASE_DIR, 'main/templates'),
                  os.path.join(BASE_DIR, 'src/users/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
