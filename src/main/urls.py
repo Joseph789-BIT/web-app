1296bda3503f5f0782d30e070be8f1e56d0a9498
Joseph Njoku <oxeejo@gmail.com>
2024-11-09 17:01:50 +0100

    initial-commit

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import main_view, home_view, list_view
+from .views import main_view, home_view, list_view, profile_view, listing_view
 
 urlpatterns = [
     path('', main_view, name='main'),
     path('home/', home_view, name='home'),
-    path('list/', list_view, name='list')
+    path('list/', list_view, name='list'),
+    path('listing/', listing_view, name='listing'),
+    path('profile/', profile_view, name='profile'),
 ]
--- src/main/views.py
+++ src/main/views.py
@@ -46,3 +46,11 @@ def list_view(request):
         listing_form = ListingForm()
         location_form = LocationForm()
         return render(request, 'views/list.html', {'listing_form': listing_form, 'location_form': location_form})
+    
+@login_required
+def listing_view(request):
+    return render(request, 'views/listing.html', {})
+
+@login_required
+def profile_view(request):
+    pass
