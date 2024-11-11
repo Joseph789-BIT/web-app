3ac3805e12ececc769ea5f87e747c07d1e44d25e
Joseph Njoku <oxeejo@gmail.com>
2024-11-11 19:13:46 +0100

    initial-commit

--- src/users/views.py
+++ src/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.contrib import messages
 from django.views import View
 
@@ -47,8 +48,11 @@ class RegisterView(View):
         else:
             messages.error(request, f'An error occured trying to register.')
             return render(request, 'views/register.html', {'register_form': register_form})
-        
+
+
+@method_decorator(login_required, name='dispatch')
 class ProfileView(View):
+
 
     def get(self, request):
         return render(request, 'views/profile.html', {})
