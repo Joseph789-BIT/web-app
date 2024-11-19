785a6963e8c23052e76acfc78c31abb4f5652719
Joseph Njoku <oxeejo@gmail.com>
2024-11-19 16:08:49 +0100

    initial-commit

--- src/users/views.py
+++ src/users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.contrib import messages
 from django.views import View
-from main.models import Listing
+from main.models import Listing, LikedListing
 from .forms import UserForm, ProfileForm, LocationForm
 
 
@@ -59,16 +59,21 @@ class ProfileView(View):
 
     def get(self, request):
         user_listings = Listing.objects.filter(seller=request.user.profile)
+        user_liked_listings = LikedListing.objects.filter(
+            seller=request.user.profile).all()
         user_form = UserForm(instance=request.user)
         profile_form = ProfileForm(instance=request.user.profile)
         location_form = LocationForm(instance=request.user.profile.location)
         return render(request, 'views/profile.html', {'user_form': user_form, 
                                                       'profile_form': profile_form, 
                                                       'location_form': location_form, 
-                                                      'user_listings': user_listings})
-    
+                                                      'user_listings': user_listings, 
+                                                      'user_liked_listing': user_liked_listings})
+
     def post(self, request):
         user_listings = Listing.objects.filter(seller=request.user.profile)
+        user_liked_listings = LikedListing.objects.filter(
+            seller=request.user.profile).all()
         user_form = UserForm(request.POST, instance=request.user)
         profile_form = ProfileForm(
             request.POST, request.FILES, instance=request.user.profile)
@@ -83,5 +88,6 @@ class ProfileView(View):
             messages.error(request, 'Error Updating Profile!')
         return render(request, 'views/profile.html', {'user_form': user_form, 
                                                       'profile_form': profile_form, 
-                                                      'location_form': location_form,
-                                                       'user_listings': user_listings})
+                                                      'location_form': location_form, 
+                                                      'user_listings': user_listings, 
+                                                      'user_liked_listing': user_liked_listings})
