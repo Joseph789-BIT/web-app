72b8019bf9364c1f2f37e574243ee008b171e065
Joseph Njoku <oxeejo@gmail.com>
2024-11-23 16:44:40 +0100

    man

--- src/showblit/settings.py
+++ src/showblit/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
